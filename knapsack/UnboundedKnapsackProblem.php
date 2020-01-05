173fab0a0c826ae0373734d04f106488e2d8b577
evgeniy <evgromr1@gmail.com>
2020-01-05 18:51:59 +0300

    Add php knapsack problem

---
+++ knapsack/UnboundedKnapsackProblem.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+class UnboundedKnapsackProblem
+{
+    /**
+     * @see https://en.wikipedia.org/wiki/Knapsack_problem
+     * @param int[] $itemSizes
+     * @psalm-param list<int> $itemSizes
+     *
+     * @return int maximum amounnt of space than can be occupied by provided set of items
+     */
+    public function solve(int $maxBackpackSize, array $itemSizes): int {
+        $matrix = [];
+        foreach ($itemSizes as $row => $itemSize) {
+            $matrix[$row] = [];
+            for($bpSize = 0; $bpSize <= $maxBackpackSize; $bpSize++) {
+                $topValue = $row === 0 ? 0 : $matrix[$row - 1][$bpSize];
+                $leftValue = $bpSize === 0 ? 0 : $matrix[$row][$bpSize - 1];
+
+                if($itemSize > $bpSize) {
+                    $matrix[$row][$bpSize] = max($topValue, $leftValue);
+                }
+
+                if($itemSize === $bpSize) {
+                    $matrix[$row][$bpSize] = max($topValue, $leftValue, $itemSize);
+                }
+
+                if($itemSize < $bpSize) {
+                    $remainingSpaceSizeUsage = $matrix[$row][$bpSize - $itemSize];
+
+                    $matrix[$row][$bpSize] = max(
+                        $topValue,
+                        $leftValue,
+                        $itemSize + $remainingSpaceSizeUsage
+                    );
+                }
+            }
+        }
+        $end = end($matrix);
+        return end($end);
+    }
+}
